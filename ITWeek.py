43a868e654b7351f9513df3cee365f45b34e01d0
run25 <t25.okano@gmail.com>
2017-06-14 01:00:51 +0900

    Revert "組込みシステム開発技術展 に出展した企業のみに変更"
    This reverts commit eb3223b1fb103fc18b1f752a50d341d394c6a9cb.

--- ITWeek.py
+++ ITWeek.py
@@ -7,10 +7,8 @@ def main():
     soup = BeautifulSoup(res.content, 'html.parser')
     companies = soup.find_all('tr')
     for company in companies:
-        exhib = company.find('td',{"class":"td_exhibitions"})
-        if exhib != None:
-            if exhib.text == "‘g‚İƒVƒXƒeƒ€ŠJ”­‹Zp“W":
-                print(company.text)
+        print(company.text)
 
 if __name__ == '__main__':
     main()
+
